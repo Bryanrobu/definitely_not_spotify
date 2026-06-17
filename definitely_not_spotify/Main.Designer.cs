167671ec3aae11ebb1abd0474ebf04101d0baafb
Bryanrobu <1211898@student.roc-nijmegen.nl>
2026-06-17 10:54:53 +0200

    updated design a small bit

--- definitely_not_spotify/Main.Designer.cs
+++ definitely_not_spotify/Main.Designer.cs
@@ -50,10 +50,9 @@
             // 
             button1.Anchor = AnchorStyles.Bottom;
             button1.AutoSize = true;
-            button1.Location = new Point(409, 524);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(358, 393);
             button1.Name = "button1";
-            button1.Size = new Size(125, 60);
+            button1.Size = new Size(109, 45);
             button1.TabIndex = 0;
             button1.Text = "Play/pause";
             button1.UseVisualStyleBackColor = true;
@@ -63,10 +62,9 @@
             // 
             button2.Anchor = AnchorStyles.Bottom;
             button2.AutoSize = true;
-            button2.Location = new Point(541, 524);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(473, 393);
             button2.Name = "button2";
-            button2.Size = new Size(125, 60);
+            button2.Size = new Size(109, 45);
             button2.TabIndex = 1;
             button2.Text = "Skip";
             button2.UseVisualStyleBackColor = true;
@@ -76,10 +74,9 @@
             // 
             button3.Anchor = AnchorStyles.Bottom;
             button3.AutoSize = true;
-            button3.Location = new Point(278, 524);
-            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Location = new Point(243, 393);
             button3.Name = "button3";
-            button3.Size = new Size(125, 60);
+            button3.Size = new Size(109, 45);
             button3.TabIndex = 2;
             button3.Text = "Previous";
             button3.UseVisualStyleBackColor = true;
@@ -89,10 +86,9 @@
             // 
             Playlists.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             Playlists.FormattingEnabled = true;
-            Playlists.Location = new Point(0, 60);
-            Playlists.Margin = new Padding(3, 4, 3, 4);
+            Playlists.Location = new Point(0, 45);
             Playlists.Name = "Playlists";
-            Playlists.Size = new Size(156, 404);
+            Playlists.Size = new Size(137, 304);
             Playlists.TabIndex = 3;
             Playlists.SelectedIndexChanged += Playlists_SelectedIndexChanged;
             // 
@@ -100,96 +96,90 @@
             // 
             Numbers.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             Numbers.FormattingEnabled = true;
-            Numbers.Location = new Point(163, 60);
-            Numbers.Margin = new Padding(3, 4, 3, 4);
+            Numbers.Location = new Point(143, 45);
             Numbers.Name = "Numbers";
-            Numbers.Size = new Size(163, 404);
+            Numbers.Size = new Size(143, 304);
             Numbers.TabIndex = 4;
             // 
             // Users
             // 
             Users.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             Users.FormattingEnabled = true;
-            Users.Location = new Point(648, 56);
-            Users.Margin = new Padding(3, 4, 3, 4);
+            Users.Location = new Point(681, 44);
             Users.Name = "Users";
-            Users.Size = new Size(137, 444);
+            Users.Size = new Size(120, 334);
             Users.TabIndex = 5;
             Users.SelectedIndexChanged += Users_SelectedIndexChanged;
             // 
             // search
             // 
-            search.Location = new Point(334, 20);
-            search.Margin = new Padding(3, 4, 3, 4);
+            search.Location = new Point(292, 15);
             search.Name = "search";
             search.PlaceholderText = "Input search ";
-            search.Size = new Size(437, 27);
+            search.Size = new Size(383, 23);
             search.TabIndex = 6;
             search.KeyUp += search_KeyUp;
             // 
             // Discover
             // 
             Discover.FormattingEnabled = true;
-            Discover.Location = new Point(334, 59);
-            Discover.Margin = new Padding(3, 4, 3, 4);
+            Discover.Location = new Point(292, 44);
             Discover.Name = "Discover";
-            Discover.Size = new Size(437, 444);
+            Discover.Size = new Size(383, 334);
             Discover.TabIndex = 7;
             // 
             // nowPlaying
             // 
             nowPlaying.BackColor = SystemColors.Window;
             nowPlaying.Enabled = false;
-            nowPlaying.Location = new Point(14, 524);
-            nowPlaying.Margin = new Padding(3, 4, 3, 4);
+            nowPlaying.Location = new Point(12, 393);
             nowPlaying.Multiline = true;
             nowPlaying.Name = "nowPlaying";
             nowPlaying.ReadOnly = true;
-            nowPlaying.Size = new Size(205, 59);
+            nowPlaying.Size = new Size(180, 45);
             nowPlaying.TabIndex = 8;
             // 
             // currentUser
             // 
             currentUser.AutoSize = true;
-            currentUser.Location = new Point(821, 524);
+            currentUser.Location = new Point(718, 393);
             currentUser.Name = "currentUser";
-            currentUser.Size = new Size(86, 20);
+            currentUser.Size = new Size(70, 15);
             currentUser.TabIndex = 9;
             currentUser.Text = "current user";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(0, 36);
+            label1.Location = new Point(0, 27);
             label1.Name = "label1";
-            label1.Size = new Size(61, 20);
+            label1.Size = new Size(49, 15);
             label1.TabIndex = 10;
             label1.Text = "Playlists";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(163, 36);
+            label2.Location = new Point(143, 27);
             label2.Name = "label2";
-            label2.Size = new Size(69, 20);
+            label2.Size = new Size(56, 15);
             label2.TabIndex = 11;
             label2.Text = "Numbers";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(778, 24);
+            label3.Location = new Point(681, 18);
             label3.Name = "label3";
-            label3.Size = new Size(44, 20);
+            label3.Size = new Size(35, 15);
             label3.TabIndex = 12;
             label3.Text = "Users";
             // 
             // logout
             // 
-            logout.Location = new Point(821, 553);
-            logout.Margin = new Padding(3, 4, 3, 4);
+            logout.Location = new Point(718, 415);
             logout.Name = "logout";
-            logout.Size = new Size(86, 31);
+            logout.Size = new Size(75, 23);
             logout.TabIndex = 13;
             logout.Text = "Log out";
             logout.UseVisualStyleBackColor = true;
@@ -197,10 +187,9 @@
             // 
             // add_playlist
             // 
-            add_playlist.Location = new Point(0, 473);
-            add_playlist.Margin = new Padding(3, 4, 3, 4);
+            add_playlist.Location = new Point(0, 355);
             add_playlist.Name = "add_playlist";
-            add_playlist.Size = new Size(157, 31);
+            add_playlist.Size = new Size(137, 23);
             add_playlist.TabIndex = 14;
             add_playlist.Text = "Add playlist";
             add_playlist.UseVisualStyleBackColor = true;
@@ -208,10 +197,9 @@
             // 
             // delete_playlist
             // 
-            delete_playlist.Location = new Point(163, 473);
-            delete_playlist.Margin = new Padding(3, 4, 3, 4);
+            delete_playlist.Location = new Point(143, 355);
             delete_playlist.Name = "delete_playlist";
-            delete_playlist.Size = new Size(163, 31);
+            delete_playlist.Size = new Size(143, 23);
             delete_playlist.TabIndex = 15;
             delete_playlist.Text = "Delete playlist";
             delete_playlist.UseVisualStyleBackColor = true;
@@ -219,9 +207,9 @@
             // 
             // Main
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(800, 450);
             Controls.Add(delete_playlist);
             Controls.Add(add_playlist);
             Controls.Add(logout);
@@ -238,7 +226,6 @@
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Main";
             Text = "Main";
             ResumeLayout(false);
